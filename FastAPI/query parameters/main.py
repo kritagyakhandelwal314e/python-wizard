bbedf88698386ce0fc6f34676597e406d7d02f07
kritagyakhandelwal314e <kritagya.khandelwal@314ecorp.com>
2021-06-25 02:10:42 +0530

    Optional Query Parameters

--- FastAPI/query parameters/main.py
+++ FastAPI/query parameters/main.py
@@ -11,6 +11,19 @@ fake_items_db = [{
   }
 ]
 
+# Query Parameters with default values
 @app.get('/items/')
 async def read_item(skip: int = 0, limit: int = 10):
   return fake_items_db[skip : limit + skip]
+
+# Optional Query Parameters
+from typing import Optional
+
+@app.get('/items/{item_id}')
+async def read_item2(item_id: str, q: Optional[str] = None):
+  value = {
+    "item_id": item_id
+  }
+  if q:
+    value["q"] = q
+  return value
